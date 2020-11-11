a71083a2c5597f54ad617b38c93e7c95f814d309
Jason Harris <harris.e.jason@gmail.com>
2020-11-11 20:06:05 +0100

    added end conditions for implicit condition

--- src/examples/test_pyddl_tower.py
+++ src/examples/test_pyddl_tower.py
@@ -1,9 +1,10 @@
-from pyddl import Domain, Problem, Action, neg, planner
+from pyddl import Domain, Problem, State, Action, neg, planner
+
+
+
 
 def problem(verbose):
-
-    domain = Domain((
-        Action("pick-up",
+    pick_up = Action("pick-up",
                parameters=(
                    ("block", "b"),
                    ("gripper", "g")
@@ -17,26 +18,38 @@ def problem(verbose):
                    ("inhand", "g", "b"),
                    neg(("free", "b")),
                    neg(("empty", "g"))
-               )),
-        Action("place_on",
-               parameters=(
-                   ("block", "b"),
-                   ("gripper", "g"),
-                   ("block", "c")
-               ),
-               preconditions=(
-                   ("inhand","g", "b"),
-                   ("free", "c")
-               ),
-               effects=(
-                   neg(("inhand", "g", "b")),
-                   neg(("free", "c")),
-                   ("free", "b"),
-                   ("empty", "g"),
-                   ("on", "b", "c")
-
                ))
+    place_on = Action("place_on",
+           parameters=(
+               ("block", "b"),
+               ("gripper", "g"),
+               ("block", "c")
+           ),
+           preconditions=(
+               ("inhand", "g", "b"),
+               ("free", "c")
+           ),
+           effects=(
+               neg(("inhand", "g", "b")),
+               neg(("free", "c")),
+               ("free", "b"),
+               ("empty", "g"),
+               ("on", "b", "c")
+
+           ))
+
+    # define domain here (possible actions)
+    domain = Domain((
+        pick_up,
+        place_on
     ))
+
+    goal =(
+            ("on", 1, 2),
+            ("on", 2, 3),
+    )
+
+    # define problem here
     prob = Problem(
         domain,
         {
@@ -49,18 +62,51 @@ def problem(verbose):
             ("free", 2),
             ("free", 3)
         ),
-        goal=(
-            ("on", 1, 2),
-            ("on", 2, 3),
-            ("empty", "g")
-        )
+        goal=goal
     )
+
+    #generate plan
     plan = planner(prob, verbose=verbose)
     if plan is None:
         print('No Plan!')
     else:
+        print(type(plan))
         for action in plan:
             print(action)
+
+    ## need to calculate the end effect of actions
+
+    for action in plan:
+        predicates = list()
+        functions = dict()
+        for predicate in action.preconditions:
+            if predicate[0] == '=':
+                functions[predicate[1]] = predicate[2]
+            else:
+                predicates.append(predicate)
+        temp_state = State(predicates, functions)
+        final_state = temp_state.apply(action)
+        print("Start_cond", temp_state.predicates)
+        print("final_cond", final_state.predicates)
+
+    def get_implicit_feasibility(plan, goal):
+
+        implied_cond = []
+
+        #plan.append(goal)
+        for i in reversed(range(0, len(plan))):
+            # check if we are at goal:
+            action = plan[i]
+            if i == len(plan) - 1:
+                implied_cond_i = [x for x in plan if x not in action.preconditions]
+            #else:
+            # for each action, get the name
+            print(action.preconditions)
+
+
+    get_implicit_feasibility(plan, goal)
+    # get the implicit feasibility
+
 
 if __name__ == '__main__':
     from optparse import OptionParser
---
+++ src/util/robustness.py
@@ -0,0 +1,13 @@
+
+
+class dummy_action:
+
+    def __init__(self, OT):
+
+
+def get_implicit_feasibility():
+
+    plan = []
+
+    for ctrlset in plan
+    # get the implicit feasibility
